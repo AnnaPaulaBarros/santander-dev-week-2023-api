2a8234f4a3c35062afa78fa3fd1b4e859507a82e
annapaulabarros <annapaula.barrosdasilva@gmail.com>
2024-07-29 19:17:53 -0300

    Melhoria do NewsDto

--- src/main/java/me/dio/controller/dto/NewsDto.java
+++ src/main/java/me/dio/controller/dto/NewsDto.java
@@ -2,13 +2,28 @@ package me.dio.controller.dto;
 
 import me.dio.domain.model.News;
 
+/**
+ * Data Transfer Object (DTO) para a entidade News.
+ * Utilizado para transferir dados entre camadas da aplicação.
+ */
 public record NewsDto(Long id, String icon, String description) {
 
+    /**
+     * Construtor que cria um NewsDto a partir de um objeto News.
+     *
+     * @param model Objeto News a ser convertido.
+     */
     public NewsDto(News model) {
         this(model.getId(), model.getIcon(), model.getDescription());
     }
 
+    /**
+     * Converte este DTO para o modelo News.
+     *
+     * @return Objeto News correspondente a este DTO.
+     */
     public News toModel() {
+        // Cria uma nova instância do modelo News e define seus atributos com base nos valores do DTO
         News model = new News();
         model.setId(this.id);
         model.setIcon(this.icon);
@@ -16,4 +31,3 @@ public record NewsDto(Long id, String icon, String description) {
         return model;
     }
 }
-
